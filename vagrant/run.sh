7e706a11a9b53b1b61dd1b5afc95c6fed6f2ed4e
Fabian von Feilitzsch <fabian@fabianism.us>
2017-10-14 12:21:20 -0400

    verify that ansible runner requirements are met (to fail early

--- vagrant/run.sh
+++ vagrant/run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
+python -c 'import netaddr'
+if [[ "$?" -ne 0 ]]; then
+ echo "You must install the python netaddr module"
+ exit 1
+fi
+
 set -e
 vagrant destroy
 vagrant up --provision
