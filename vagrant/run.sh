1587f068978600379a1d120f8e28507ee7939fae
Fabian von Feilitzsch <fabian@fabianism.us>
2017-10-14 12:03:57 -0400

    lots of cleanup, fixed networking defaults

--- vagrant/run.sh
+++ vagrant/run.sh
@@ -6,4 +6,4 @@ vagrant up --provision
 
 sed '/\[foreman\]/a foreman.example.org' ../hosts.example > ../inventory/hosts
 
-(cd .. ; ansible-playbook -i inventory playbooks/foreman.yml -e "authorized_keys=$(cat ~/.ssh/id_rsa.pub)" -e "tftp_servername=192.168.17.11" -e "dns_interface=eth1")
+(cd .. ; ansible-playbook -i inventory playbooks/foreman.yml -e "foreman_dns_interface=eth1")
